1167b966c18b9c3bd6660437780063dbc4f3d4b1
jcpinheiro <jcpinheiro@ifma.edu.br>
2020-01-24 17:56:33 -0300

    teste travis 9

--- src/test/java/com/example/estoque/EstoqueApplicationTests.java
+++ src/test/java/com/example/estoque/EstoqueApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.estoque;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class EstoqueApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
