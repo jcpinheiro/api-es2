d2f6575abef3d9f9d1c7c57b7f6d385aaadfc0e8
jcpinheiro <jcpinheiro@ifma.edu.br>
2020-01-27 17:49:10 -0300

    integracao com travis CI - teste 10

--- src/main/resources/db/migration/V01__cria_tabela_produto.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE produto (
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(100) NOT NULL,
-descricao TEXT NULL,
-valor DECIMAL(12,2) NULL,
-PRIMARY KEY (id));
--- src/main/resources/db/migration/V02__insere_registros_em_produto.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO produto (nome, descricao, valor) VALUES ('Notebook Dell i7', 'Notebook core i7', '3000');
-INSERT INTO produto (nome, descricao, valor) VALUES ('Macbook Pro', 'Macbook com tela de retina', '5000');
