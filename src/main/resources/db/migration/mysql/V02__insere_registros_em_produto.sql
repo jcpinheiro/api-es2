78870896cf42c7a969c3d99a08cde1b6ce1741f3
jcpinheiro <jcpinheiro@ifma.edu.br>
2020-01-27 17:40:28 -0300

    integracao com travis CI - teste 06

---
+++ src/main/resources/db/migration/mysql/V02__insere_registros_em_produto.sql
@@ -0,0 +1,2 @@
+INSERT INTO produto (nome, descricao, valor) VALUES ('Notebook Dell i7', 'Notebook core i7', '3000');
+INSERT INTO produto (nome, descricao, valor) VALUES ('Macbook Pro', 'Macbook com tela de retina', '5000');
